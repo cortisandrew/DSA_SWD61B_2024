40b21eeb7f7f3916f0545c775821ec81bcb16c4a
Andrew Cortis <Andrew.Cortis@mcast.edu.mt>
2024-04-25 14:34:59 +0200

    .

---
+++ AlgorithmExamples/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+int n = 100;
+
+for (int i = 0; i < n; i++)
+{
+    int innerLoopWork = 0;
+    for (int j = i; j < n; j++)
+    {
+        innerLoopWork++; 
+    }
+    // innerLoopWork = n - i + 1;
+
+    Console.Write(innerLoopWork + ",");
+}
