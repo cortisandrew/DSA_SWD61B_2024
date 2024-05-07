744e6eb6d3b162068cde157af099c2b4791d2808
Andrew Cortis <Andrew.Cortis@mcast.edu.mt>
2024-05-07 10:58:03 +0200

    Added Heap class

---
+++ TreeProject2/BinayMaxHeap.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TreeProject2
+{
+    public class BinaryMaxHeap<T>
+    {
+        List<KeyValuePair<int, T>> data = new List<KeyValuePair<int, T>>();
+
+        private int LeftChildIndex(int index)
+        {
+            throw new NotImplementedException();
+        }
+
+        private int RightChildIndex(int index)
+        {
+            throw new NotImplementedException();
+        }
+
+        private int ParentIndex(int index) { throw new NotImplementedException(); }
+
+
+        public void Add(int key, T value)
+        {
+
+        }
+
+        public T RemoveMax()
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
